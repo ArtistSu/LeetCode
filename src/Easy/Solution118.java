8554a30e0b4d9cff0df885ec3a4a58bf157697c1
AllenSuWindows <syl960918@163.com>
2021-01-09 20:25:42 +0800

    202101092025PM

--- src/Easy/Solution118.java
+++ src/Easy/Solution118.java
@@ -1,5 +1,7 @@
 package Easy;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -12,7 +14,29 @@ import java.util.List;
  */
 public class Solution118 {
     public List<List<Integer>> generate(int numRows) {
-        
+        // initial the result list
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows <= 0) {
+            return result;
+        }
+
+
+        result.add(new ArrayList<>());
+        result.get(0).add(1);
+
+        for(int numRow = 1; numRow < numRows; numRow++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = result.get(numRow-1);
+            for (int j = 0; j <= numRow; j++) {
+                if (j == 0 || j == numRow) {
+                    row.add(1);
+                    continue;
+                }
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            result.add(row);
+        }
+        return result;
     }
 
 }
