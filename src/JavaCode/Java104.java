34b0b77e862c8b31fe3f74042a6040b87d68ffa9
ArtistSuMac <syl960918@163.com>
2021-10-18 16:06:25 +0100

    202110181605PMLE

--- src/JavaCode/Java104.java
+++ src/JavaCode/Java104.java
@@ -28,17 +28,23 @@ public class Java104 {
 
     }
 
-    int leftDepth = 0;
-    int rightDepth = 0;
-    // "Top - down" Time Complexity: O(n) Space Complexity: O(n)
+    // "Bottom - up" Time Complexity: O(1) Space Complexity: O(n)
     public int maxDepth(TreeNode root) {
-
-        // If the node is leaf node, return 0;
         if (root == null) {
             return 0;
         }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
 
 
-        return Math.max(leftDepth,rightDepth);
+    public static void main(String[] args) {
+        Java104.TreeNode a5 = new Java104().new TreeNode(5, null, null);
+        Java104.TreeNode a4 = new Java104().new TreeNode(4, null, null);
+        Java104.TreeNode a3 = new Java104().new TreeNode(3, null, a5);
+        Java104.TreeNode a2 = new Java104().new TreeNode(2, a4, null);
+        Java104.TreeNode a1 = new Java104().new TreeNode(1, a2, a3);
+        System.out.println(new Java104().maxDepth(a1));
     }
 }
