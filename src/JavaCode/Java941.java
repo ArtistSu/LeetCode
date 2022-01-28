a812e8c1daf5f975c675734ebfb1c0f295017c78
ArtistSuMac <syl960918@163.com>
2022-01-28 12:02:22 +0000

    histroy code

--- src/JavaCode/Java941.java
+++ src/JavaCode/Java941.java
@@ -1,23 +1,34 @@
 package JavaCode;
 
 public class Java941 {
+    /**
+     *
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     *
+     * @param arr
+     * @return
+     * @author: Allen Su
+     */
     public boolean validMountainArray(int[] arr) {
-
-        int length = arr.length;
+        if(arr.length <= 2){
+            return false;
+        }
+        int step = arr.length;
         int i = 0;
 
-        while (i + 1 < length && arr[i] < arr[i + 1]) {
+        while(i+1<step && arr[i] < arr[i+1]){
             i++;
         }
 
-        if (i == 0 || i == length) {
+        if(i==0 || i == step-1){
             return false;
         }
 
-        while (i + 1 < length && arr[i] > arr[i + 1]) {
+        while(i+1<step && arr[i]> arr[i+1]){
             i++;
         }
-        return i == length;
-
+        return i == step-1;
     }
+
 }
--- test/temp.java
+++ test/temp.java
@@ -2,5 +2,9 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 
 public class temp {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a>>2);
+    }
 
 }
