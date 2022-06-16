42a8a4eadb23e0ff72d48d258ebb22d20dbc040b
ArtistSuMac <syl960918@163.com>
2022-06-16 18:52:54 +0100

    202206016 UK

--- src/JavaCode/Java1089.java
+++ src/JavaCode/Java1089.java
@@ -1,6 +1,36 @@
 package JavaCode;
 
+import java.util.Stack;
+
 public class Java1089 {
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,0,0,1,0,2};
+        new Java1089().duplicateZeros2(arr);
+    }
+
+    /**
+     * Time Complexity: O(n) {@code n} is the length of {@param arr}
+     * Space Complexity: O(1)
+     * @mthod Simulation
+     * @topic Duplicate Zeros
+     * @author ArtistS
+     * @param arr
+     */
+    public void duplicateZeros2(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < arr.length;i++){
+            if(sb.length() == arr.length){
+                break;
+            }
+            sb.append(arr[i]);
+            if(arr[i] == 0){
+                sb.append(0);
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sb.charAt(i)-'0';
+        }
+    }
     public void duplicateZeros(int[] arr) {
         int arrLength = arr.length;
         int breakpoint = 0;
