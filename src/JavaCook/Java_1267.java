06c6938e92d524ce71f3e1c074369eef5ba8ec62
ArtistS-win <sylgg0918@gmail.com>
2025-01-23 18:42:32 +0000

    2025-01-23

---
+++ src/JavaCook/Java_1267.java
@@ -0,0 +1,40 @@
+package JavaCook;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author ArtistS
+ * @tag
+ * @prb Time complexity:
+ * Space complexity:
+ */
+public class Java_1267 {
+    public int countServers(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+
+        int[] rows = new int[m];
+        int[] columns = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rows[i] += 1;
+                    columns[j] += 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (rows[i] >= 2 || columns[j] >= 2)) {
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
